1203b47e3abbc358af5d1502bcd020cf0d126bb1
minmin0530 <izumiyoshiki@icloud.com>
2020-04-28 08:29:13 +0900

    Initial Commit

---
+++ MentaMVVM001/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MentaMVVM001
+//
+//  Created by Yoshiki Izumi on 2020/04/28.
+//  Copyright © 2020 Yoshiki Izumi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
